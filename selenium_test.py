57cdaa66f007c28c7243a9d76c2febcff5a2ecbe
AD\hirang <semp0110@naver.com>
2022-04-21 15:18:49 +0900

    기본셋팅

---
+++ selenium_test.py
@@ -0,0 +1,28 @@
+import selenium
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+URL = 'https://shopping.naver.com/home/p/index.naver'
+검색어 = "키오시아 EXCERIA M.2 NVMe"
+print("aaa")
+driver = webdriver.Chrome(executable_path='chromedriver')
+driver.get(url=URL)
+print(driver.current_url)
+
+inputElement = driver.find_element(By.XPATH,"//*[@id='autocompleteWrapper']/input[1]")
+inputElement.send_keys(검색어)
+
+# 대기
+sleep(3)
+driver.close()
+
+
